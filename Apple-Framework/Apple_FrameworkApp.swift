68e8a2b22f71ce60e222ee544a9c0f2270611a33
iamvikramjeet1910 <chhotu123kota@gmail.com>
2024-03-08 19:57:03 +0530

    Initial Commit

---
+++ Apple-Framework/Apple_FrameworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameworkApp.swift
+//  Apple-Framework
+//
+//  Created by Vikram Kumar on 08/03/24.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FrameworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
